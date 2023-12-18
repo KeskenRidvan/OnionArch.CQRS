19bb8457340f0a6a00b9b9dd35fddd239534d065
Ridvan Kesken <kesken.dev@gmail.com>
2023-12-19 00:23:04 +0300

    Basic entities have been created.

---
+++ Core/OnionArch.Domain/Entities/Brand.cs
@@ -0,0 +1,15 @@
+﻿using OnionArch.Domain.Common;
+
+namespace OnionArch.Domain.Entities;
+
+public class Brand : EntityBase
+{
+	public Brand() { }
+	public Brand(string name)
+	{
+		Name = name;
+	}
+	public required string Name { get; set; }
+
+	public ICollection<Product> Products { get; set; }
+}
